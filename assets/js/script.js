60053738ab7182bcaf886b1f2958d65a5b3361f0
thedimebag <magezashalom0@gmail.com>
2024-08-23 10:19:42 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,48 @@
 'use strict';
 
+
+
 /**
- * Add event on elements
+ * add event on elements
  */
-const addEventOnElem = (elems, type, callback) => {
-  if (elems) {
-    if (elems.length > 1) {
-      elems.forEach(elem => elem.addEventListener(type, callback));
-    } else {
-      elems.addEventListener(type, callback);
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
     }
+  } else {
+    elem.addEventListener(type, callback);
   }
 }
 
+
+
 /**
- * Navbar toggle
+ * navbar toogle
  */
+
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
 
-const toggleNavbar = () => {
+const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
+
+
 /**
- * Active header & back to top button when window scroll down to 100px
+ * active header & back top btn when window scroll down to 100px
  */
+
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-const activeElemOnScroll = () => {
+const activeElemOnScroll = function () {
   if (window.scrollY > 100) {
     header.classList.add("active");
     backTopBtn.classList.add("active");
@@ -43,21 +52,17 @@ const activeElemOnScroll = () => {
   }
 }
 
-const debounce = (callback, delay = 100) => {
-  let timer;
-  return () => {
-    clearTimeout(timer);
-    timer = setTimeout(callback, delay);
-  }
-}
+addEventOnElem(window, "scroll", activeElemOnScroll);
 
-addEventOnElem(window, "scroll", debounce(activeElemOnScroll));
+
 
 /**
- * Filter functionality
+ * filter functionality
  */
+
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 const filterItems = document.querySelectorAll("[data-filter]");
+
 let lastClickedBtn = filterBtn[0];
 
 const filter = function () {
@@ -65,40 +70,13 @@ const filter = function () {
   this.classList.add("active");
   lastClickedBtn = this;
 
-  filterItems.forEach(item => {
-    item.style.display = item.dataset.filter === this.dataset.filterBtn ? "block" : "none";
-  });
+  for (let i = 0; i < filterItems.length; i++) {
+    if (filterItems[i].dataset.filter === this.dataset.filterBtn) {
+      filterItems[i].style.display = "block";
+    } else {
+      filterItems[i].style.display = "none";
+    }
+  }
 }
 
 addEventOnElem(filterBtn, "click", filter);
-
-/**
- * Video popup functionality
- */
-const playVideoBtn = document.getElementById("play-video-btn");
-const videoPopup = document.getElementById("video-popup");
-const closeBtn = document.getElementById("close-btn");
-const overlayPopup = document.getElementById("overlay");
-
-const showVideoPopup = () => videoPopup.classList.remove("hidden");
-const hideVideoPopup = () => videoPopup.classList.add("hidden");
-
-addEventOnElem(playVideoBtn, "click", showVideoPopup);
-addEventOnElem(closeBtn, "click", hideVideoPopup);
-addEventOnElem(overlayPopup, "click", hideVideoPopup);
-
-/**
- * Back to top button functionality
- */
-const backToTopBtn = document.getElementById("back-to-top");
-
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-
-  
-}
-
-addEventOnElem(backToTopBtn, "click", scrollToTop);
